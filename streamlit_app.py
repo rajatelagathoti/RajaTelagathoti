af3405a2d9c56275281b77803823bb5ada2d77dc
Raja Telagatoti <rajatelagathoti19@gmail.com>
2026-07-04 11:31:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
-
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+from flask import Flask, request
+app = Flask(__name__)
+@app.route("/")
+def home():
+  return open("YouTube.html").read()
+app.run(debug = "True")
